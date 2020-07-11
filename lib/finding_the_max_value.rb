974a48b768ff229ae1eaa06364a1bea30d61ffa1
John Easter <john@owltestprep.com>
2020-07-11 13:48:55 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
-def find_max_value(array)
-  # Add your solution here
+def find_max_value(array = [1, 2, 3, 4, 5, 6])
+  counter = 0
+  found_max = -1
+  while counter < array.length do
+    if found_max < array[counter]
+      found_max = array[counter]
+    end
+  counter += 1
+  end
+  found_max
 end
